f9911154f696c0f2bd6a1b3ae3ab478e28dd12bf
18598925736 <545679452@qq.com>
2022-11-08 17:07:27 +0800

    Update about_the_one_vm.dart

--- lib/page/about_the_one/vm/about_the_one_vm.dart
+++ lib/page/about_the_one/vm/about_the_one_vm.dart
@@ -1,70 +1,62 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_easyloading/flutter_easyloading.dart';
-import 'package:oktoast/oktoast.dart';
-import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:smart_diary/db/about_the_one_db_provider.dart';
+import 'package:smart_diary/db/comm.dart';
+import 'package:smart_diary/db/db_core.dart';
+import 'package:smart_diary/db/entity/the_one_topic.dart';
+import 'package:smart_diary/page/base/loading_view.dart';
 
-import 'comm/app_info_provider.dart';
-import 'comm/const.dart';
-import 'comm/tag_cache.dart';
-import 'db/db_core.dart';
-import 'page/main_page.dart';
+import '../../../db/entity/the_one.dart';
 
-/// 入口函数
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  // 为了提高图片item在列表中快速滑动时重新加载的流畅度
-  PaintingBinding.instance.imageCache.maximumSize = 10000; // 2000 entries
-  PaintingBinding.instance.imageCache.maximumSizeBytes = 300 << 20; //
-  AppInfoProvider().init();
-  await initDb();
-  await TagCachePool().initCache();
-  runApp(const MyApp());
-  // configLoading();
-}
+class AboutTheOneVm extends ChangeNotifier {
+  final AboutTheOneDbProvider _provider = AboutTheOneDbProvider();
 
-Future initDb() async {
-  // 初始化DB
-  await DbCore.open();
-  // 初始化测试数据
-  SharedPreferences sp = await SharedPreferences.getInstance();
-  bool? s = sp.getBool(firstTimeRunningTag) ?? false;
-  if (s == false) {
-    await sp.setBool(firstTimeRunningTag, true);
-    // await DbCore.initTestData();
+  List<TheOne> dataList = [];
+
+  LoadingStatus loadingStatus = LoadingStatus.loading;
+
+  Future<int> insert(String taName) async {
+    return await _provider.insertOne(TheOne(name: taName));
   }
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  Future<void> update(TheOne theOne) async {
+    await _provider.update(theOne);
+    queryAll();
+  }
 
-  @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [ChangeNotifierProvider.value(value: AppInfoProvider())],
-      child: Consumer<AppInfoProvider>(
-        builder: (context, v, _) {
-          return OKToast(
-              child: MaterialApp(
-            debugShowCheckedModeBanner: false,
-            home: const MainPage(title: '我的备忘录'),
-            builder: EasyLoading.init(),
-            theme: ThemeData(
-                // 底色
-                primaryColor: v.bgColor,
-                // 字色
-                textSelectionColor: v.textColor,
-                // 主色调
-                buttonColor: v.mainColor,
-                // 字体库
-                fontFamily: v.fontFamily,
-                // 取消所有的水波纹
-                highlightColor: Colors.transparent,
-                //取消所有的水波纹
-                splashColor: Colors.transparent),
-          ));
-        },
-      ),
-    );
+  Future<int> delete(TheOne theOne) async {
+    int res = await _provider.delete(theOne.id ?? -1);
+    if (res > 0) {
+      queryAll();
+    }
+    return res;
+  }
+
+  Future<int> deleteTopic(int id) async {
+    int res = await _provider.deleteTopic(id);
+    if (res > 0) {
+      queryAll();
+    }
+    return res;
+  }
+
+  Future<int> updateTopic(TheOneTopic topic) async {
+    return await DbCore.db.update(tableAboutTheOneTopic, topic.toJson(), where: '$columnAboutTheOneTopicId = ?', whereArgs: [topic.id]);
+  }
+
+  Future<void> queryAll() async {
+    loadingStatus = LoadingStatus.loading;
+    notifyListeners();
+
+    List<TheOne> s = await _provider.queryAll();
+    dataList.clear();
+    dataList.addAll(s.reversed.toList());
+
+    if (dataList.isEmpty) {
+      loadingStatus = LoadingStatus.loadingSucButEmpty;
+    } else {
+      loadingStatus = LoadingStatus.loadingSuc;
+    }
+
+    notifyListeners();
   }
 }
