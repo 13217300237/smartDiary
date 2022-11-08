c0e413f289d72c667ac9c15f34e40bdf1348c373
18598925736 <545679452@qq.com>
2022-11-08 16:19:44 +0800

    Create const.dart

---
+++ lib/comm/const.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'package:event_bus/event_bus.dart';
+
+const String firstTimeRunningTag = 'firstTimeRunning';
+
+const int minutesCountToShowJustNow = 5; // 如果事件的创建事件与当前时间差距是5分钟以内，就不显示具体时间，而是显示 刚才
+
+int assetMaxCount = 4; // 列表中事件卡片图片显示的最大张数
+
+String strAll = '全部';
+
+const availableColors = [
+  Colors.red,
+  Colors.pink,
+  Colors.purple,
+  Colors.blue,
+  Colors.lightBlue,
+  Colors.cyan,
+  Colors.amber,
+  Colors.orange,
+];
+
+EventBus eventBus = EventBus();
